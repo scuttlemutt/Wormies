ae962ff9ee1bc5d12d01bb357e846e509f814af1
Jonathan <extremejon@comcast.net>
2017-07-20 01:47:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jon_2/wormies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jon_2.wormies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
