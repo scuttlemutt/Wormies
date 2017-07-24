7ec050bb6d9039946a0451b3ead088da8bdd4c90
Jonathan <extremejon@comcast.net>
2017-07-23 23:17:33 -0500

    Worked on threading stuff and the score view

--- app/src/main/java/com/example/jon_2/wormies/GameplayWindow.java
+++ app/src/main/java/com/example/jon_2/wormies/GameplayWindow.java
@@ -3,7 +3,9 @@ package com.example.jon_2.wormies;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 
 
@@ -24,22 +26,42 @@ public class GameplayWindow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay_window);
 
+        wormHistory = new ArrayDeque();
 
-        wormHistory.push(new Point(10,10));
+        wormHistory.add( new Point(10,10));
+        wormPosition = new Point(10,10);
         setDirection (Direction.DOWN);
         score=0;
 
+        //https://stackoverflow.com/questions/5161951/android-only-the-original-thread-that-created-a-view-hierarchy-can-touch-its-vi
         new Thread() {
             @Override public void run() {
                 while (true) {
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(500);
                         advance();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                update();
+
+                            }
+                        });
+
+
+
                     } catch ( InterruptedException e ) {}
                 }
             }
         }.start();
 
+    }
+
+    private void update() {
+
+        score++;
+        TextView scoreView= (TextView) findViewById(R.id.scoreView);
+        scoreView.setText("Score:" +score);
     }
 
     public void setDirection(Direction dir){
@@ -64,7 +86,7 @@ public class GameplayWindow extends AppCompatActivity {
 
         }
 
-        if(wormHistory.size()>score){
+        if(wormHistory.size()-1>score){
             wormHistory.removeLast();
         }
 
--- app/src/main/java/com/example/jon_2/wormies/MainActivity.java
+++ app/src/main/java/com/example/jon_2/wormies/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.jon_2.wormies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("aaaa \n aaaaa");
     }
 
+    public void startGame(View view){
+        System.out.println("start");
+
+        Intent intent = new Intent(getBaseContext(), GameplayWindow.class);
+        //intent.putExtra("GAME_DATA", gameData);
+        startActivity(intent);
+    }
 
 
 }
