a3065742c75dc471e3c5fcf1245b330c6deea70d
Jonathan <extremejon@comcast.net>
2017-07-25 15:56:04 -0500

    Experimenting with graphics, going to have to do more research before I have a chance of implementing them.

--- app/src/main/java/com/example/jon_2/wormies/GameplayWindow.java
+++ app/src/main/java/com/example/jon_2/wormies/GameplayWindow.java
@@ -3,6 +3,7 @@ package com.example.jon_2.wormies;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayDeque;
@@ -18,6 +19,7 @@ public class GameplayWindow extends AppCompatActivity {
     int score;
     Direction wormDirection;
     Point  wormPosition;
+    Point foodLocation;
     Deque<Point> wormHistory;
 
 
@@ -26,12 +28,15 @@ public class GameplayWindow extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay_window);
 
+       // startGame();
         wormHistory = new ArrayDeque();
 
         wormHistory.add( new Point(10,10));
         wormPosition = new Point(10,10);
-        setDirection (Direction.DOWN);
+        wormDirection = Direction.DOWN;
         score=0;
+
+
 
         //https://stackoverflow.com/questions/5161951/android-only-the-original-thread-that-created-a-view-hierarchy-can-touch-its-vi
         new Thread() {
@@ -62,10 +67,27 @@ public class GameplayWindow extends AppCompatActivity {
         score++;
         TextView scoreView= (TextView) findViewById(R.id.scoreView);
         scoreView.setText("Score:" +score);
+        TextView dirView= (TextView) findViewById(R.id.dirView);
+        dirView.setText("dir:" +wormDirection.toString());
     }
 
-    public void setDirection(Direction dir){
-        this.wormDirection=dir;
+    public void setDirection(View view){
+        switch (view.getId()) {
+            case  R.id.upButton:
+                wormDirection=Direction.UP;
+                break;
+            case R.id.downButton:
+                wormDirection=Direction.DOWN;
+                break;
+            case R.id.leftButton:
+                wormDirection=Direction.LEFT;
+                break;
+            case R.id.rightButton:
+                wormDirection=Direction.RIGHT;
+                break;
+
+
+        }
     }
 
     //Add old position to history and advance player toward currect direction
--- app/src/main/java/com/example/jon_2/wormies/MainActivity.java
+++ app/src/main/java/com/example/jon_2/wormies/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.jon_2.wormies;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("aaaa \n aaaaa");
+
+        Bitmap b = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+
+        ShapeDrawable mDrawable;
+        mDrawable = new ShapeDrawable(new OvalShape());
+        // If the color isn't set, the shape uses black as the default.
+        mDrawable.getPaint().setColor(0xff74AC23);
+        // If the bounds aren't set, the shape can't be drawn.
+        mDrawable.setBounds(10,10,30,30);
+
+        mDrawable.draw(c);
+
     }
 
     public void startGame(View view){
